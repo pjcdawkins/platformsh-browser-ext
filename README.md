d38b89cb3afbdcb42216e93f4de135dbc08d9a18
Patrick Dawkins <pjcdawkins@gmail.com>
2015-07-30 17:18:01 -0400

    Initial commit from Google example

---
+++ README.md
@@ -0,0 +1,1 @@
+Experimental, incomplete, unofficial, unstable, not recommended
---
+++ popup.js
@@ -0,0 +1,82 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    var tab = tabs[0];
+
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var url = tab.url;
+
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+
+  // Most methods of the Chrome extension APIs are asynchronous. This means that
+  // you CANNOT do something like this:
+  //
+  // var url;
+  // chrome.tabs.query(queryInfo, function(tabs) {
+  //   url = tabs[0].url;
+  // });
+  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+}
+
+/**
+ * @param {string} url - The current tab URL.
+ * @param {function(string,number,number)} callback - Called when info has
+ *   been found.
+ * @param {function(string)} errorCallback - Called when the there is an error.
+ *   The callback gets a string that describes the failure reason.
+ */
+function getInfo(url, callback, errorCallback) {
+  // @todo
+  var info = 'Lorem ipsum';
+
+  callback(info);
+}
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  getCurrentTabUrl(function(url) {
+    // Put the image URL in Google search.
+    renderStatus('Getting info for ' + url);
+
+    getInfo(url, function(info) {
+
+      renderStatus('Done');
+      document.getElementById('info-result').innerHTML = info;
+
+    }, function(errorMessage) {
+      renderStatus('Cannot get info: ' + errorMessage);
+    });
+  });
+});
